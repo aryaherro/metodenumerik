b3b5436b662216edb9f2dd9778dba13258a235c1
aryaherro <aryaherro@yahoo.co.id>
2019-12-01 16:53:19 +0700

    tes gui

--- src/Gui/GuiSecant.java
+++ src/Gui/GuiSecant.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package MetodeNumerik;
+package Gui;
 
 import javax.swing.JButton;
 import javax.swing.JEditorPane;
@@ -12,18 +12,25 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
+import MetodeNumerik.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author Ajeng
  */
-public class Tampilan extends javax.swing.JFrame {
+public class GuiSecant extends javax.swing.JFrame {
 
     private static final long serialVersionUID = 1L;
     /**
      * Creates new form Tampilan
      */
-    public Tampilan() {
+    public GuiSecant() {
         initComponents();
     }
 
@@ -79,7 +86,6 @@ public class Tampilan extends javax.swing.JFrame {
             }
         });
 
-        jTable1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {"", null, null, null, null},
@@ -119,6 +125,10 @@ public class Tampilan extends javax.swing.JFrame {
                 "iterasi", "a", "b", "c", "error"
             }
         ));
+        jTable1.setMaximumSize(new java.awt.Dimension(2147483647, 0));
+        jTable1.setMinimumSize(new java.awt.Dimension(30, 0));
+        jTable1.setPreferredSize(new java.awt.Dimension(150, 0));
+        jTable1.setRowHeight(20);
         jScrollPane2.setViewportView(jTable1);
 
         jLabel3.setText("Masukkan iterasi maksimum : ");
@@ -183,49 +193,58 @@ public class Tampilan extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 800, Short.MAX_VALUE)
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 649, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(pangkatMaks, javax.swing.GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel4))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(10, 10, 10)
+                                .addComponent(jLabel3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jLabel4)))
                         .addGap(14, 14, 14)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(toleransiError)
-                            .addComponent(iterasiMaks))
-                        .addGap(51, 51, 51)
-                        .addComponent(hitung))
+                            .addComponent(iterasiMaks, javax.swing.GroupLayout.DEFAULT_SIZE, 51, Short.MAX_VALUE))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(pangkatMaks, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(71, 71, 71)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(63, 63, 63)
+                        .addComponent(hitung)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 274, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(47, 47, 47))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel1)
                             .addComponent(pangkatMaks, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGap(6, 6, 6)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel3)
-                            .addComponent(iterasiMaks, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(hitung)))
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(toleransiError, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 76, Short.MAX_VALUE)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 438, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(iterasiMaks, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel4)
+                            .addComponent(toleransiError, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                        .addComponent(hitung)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         getAccessibleContext().setAccessibleDescription("");
@@ -375,13 +394,29 @@ public class Tampilan extends javax.swing.JFrame {
 
     private void hitungMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_hitungMouseClicked
                 // TODO add your handling code here:
-        //MetodeSecant MS = new MetodeSecant(Integer.parseInt(pangkatMaks.getText()), Integer.parseInt(iterasiMaks.getText()), Double.parseDouble(toleransiError.getText()));
-        //MS.inputRumus();
         
     }//GEN-LAST:event_hitungMouseClicked
 
     private void hitungActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hitungActionPerformed
         // TODO add your handling code here:
+        MetodeSecant MS = new MetodeSecant(Integer.parseInt(pangkatMaks.getText()), Integer.parseInt(iterasiMaks.getText()), Double.parseDouble(toleransiError.getText()));
+        MS.inputRumus();
+        Rumus.setText(MS.cetakRumus(MS.getJumlahPangkat()));
+        MS.inputAcakAB(JOptionPane.showInputDialog(null, "Ingin Menginput manual nilai x" + MS.superscript(1) + " dan nilai x" + MS.superscript(2) + " (y/n): "));
+        
+        DefaultTableModel modelTable = (DefaultTableModel) jTable1.getModel();
+        Object[] atributBaris;
+        try {
+            var baris = MS.langkahSecant(); //harus dikasih try catch
+            var listBaris = baris.iterator();
+            while(listBaris.hasNext()){
+                atributBaris = (Object[]) listBaris.next();
+                modelTable.addRow(atributBaris);
+            }
+            jTable1.setModel(modelTable);
+        } catch (Exception ex) {
+            Logger.getLogger(GuiSecant.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }//GEN-LAST:event_hitungActionPerformed
 
     /**
@@ -401,15 +436,16 @@ public class Tampilan extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Tampilan.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GuiSecant.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
         
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> {
-            new Tampilan().setVisible(true);
+            new GuiSecant().setVisible(true);
         });
     }
 
--- src/MetodeNumerik/MetodeSecant.java
+++ src/MetodeNumerik/MetodeSecant.java
@@ -6,6 +6,7 @@ package MetodeNumerik;
  * and open the template in the editor.
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -17,53 +18,29 @@ public class MetodeSecant extends MetodeNumerik{
     public MetodeSecant(int jumlahPangkat, int ulangMaks, double erTol) {
         super(jumlahPangkat, ulangMaks, erTol);
     }
-    /**
-     * @param args the command line arguments
-     */
     
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Scanner scn = new Scanner(System.in);
-        Scanner scnInt = new Scanner(System.in);
-        Scanner scnDouble = new Scanner(System.in);
-        
-        System.out.print("\nMasukkan pangkat maksimal : ");
-        int x = scnInt.nextInt();
-        
-        System.out.print("Masukkan jumlah iterasi maksimum yang dilakukan : ");
-        int y = scnInt.nextInt();
-        
-        System.out.print("Masukkan toleransi maksimum : ");
-        double z = scnDouble.nextDouble();
-        
-        MetodeSecant MS = new MetodeSecant(x, y, z);
-                
-        MS.inputRumus();
-        
-        System.out.println("\nRumus : " + MS.cetakRumus(MS.getJumlahPangkat()));
-        MS.cetakRumus(MS.getJumlahPangkat());
-        
-        System.out.print("\nIngin Menginput manual nilai x" + MS.superscript(1) + " dan nilai x" + MS.superscript(2) + " (y/n): ");
-        String input = scn.nextLine();
-                
-        MS.inputAcakAB(input);
-        
-        System.out.println("\nIterasi\t\tx₁\t\tx₂\t\tx₃");
-        
-        int i = 0;
-        do{
-            System.out.printf((i+1) + "\t\t%.6f\t%.6f",MS.getA(),MS.getB());
-            MS.setC(MS.rumusSecant(MS.getA(), MS.getB()));
-            System.out.printf("\t%.6f\n",MS.getC());
-            MS.setA(MS.getB());
-            MS.setB(MS.getC());
-            i++;
-        }while((i < MS.getUlangMaks()) && (!(Double.isNaN(MS.getB()))) && !(((MS.getC() - MS.getA()) <= MS.getErTol()) && ((MS.getC() - MS.getA()) >= 0)));
-
-        System.out.printf("\nNilai akar dari persamaan " + MS.cetakRumus(MS.getJumlahPangkat()) + " = 0 terdapat pada iterasi ke-" + i + " adalah %.6f\n",MS.getA());
-    }
     public double rumusSecant(double newA,  double newB){
         return (newB - ((perhitunganY(newB, getJumlahPangkat()) * (newB - newA)) / (perhitunganY(newB, getJumlahPangkat()) - perhitunganY(newA, getJumlahPangkat()))));
+    }
+    
+    @SuppressWarnings("unchecked")
+    public ArrayList langkahSecant(){
+        ArrayList list = new ArrayList<>();
+        Object[] alay = new Object[5];
+        int i = 0;
+        do{
+            alay[0] = i+1;
+            alay[1] = getA();
+            alay[2] = getB();
+            setC(rumusSecant(getA(), getB()));
+            alay[3] = getC();
+            setA(getB());
+            setB(getC());
+            alay[4] = getC() - getA();
+            i++;
+            list.add(alay);
+        }while((i < getUlangMaks()) && (!(Double.isNaN(getB()))) && !(((getC() - getA()) <= getErTol()) && ((getC() - getA()) >= 0)));
+        return list;
+        //System.out.printf("\nNilai akar dari persamaan " + cetakRumus(getJumlahPangkat()) + " = 0 terdapat pada iterasi ke-" + i + " adalah %.6f\n",getA());
     }
 }
